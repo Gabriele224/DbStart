f24612ed18dfed78b65e7e10e0fbabea9cc9da3c
Gabriele224 <gabryricca2001@gmail.com>
2026-09-19 16:18:08 +0200

    Demo.py

--- DbDiarioDiabeteST2/Demo.py
+++ DbDiarioDiabeteST2/Demo.py
@@ -178,7 +178,7 @@ data_inizio_glice = st.date_input("Data inizio")
 data_fine_glice = st.date_input("Data fine")
 
 df_glice = db_Pasto[
-    (db_Pasto["username"] == username)
+    (db_Pasto["username"] == username_pasto)
     & (pd.to_datetime(db_Pasto["data"]).dt.date >= data_inizio_glice)
     & (pd.to_datetime(db_Pasto["data"]).dt.date <= data_fine_glice)
 ]
