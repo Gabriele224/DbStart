b638f87674388caa5e91936fded70e37cb7cba41
Gabriele224 <gabryricca2001@gmail.com>
2026-09-19 16:10:39 +0200

    Demo.py

--- DbDiarioDiabeteST2/Demo.py
+++ DbDiarioDiabeteST2/Demo.py
@@ -190,8 +190,8 @@ st.subheader("Media Peso e Massa")
 
 username_pesomassa = st.selectbox("Utente", db_pesoPersonale["username"].unique(), key="utente_pesomassa")
 
-data_inizio_pm = st.date_input("Data inizio")
-data_fine_pm = st.date_input("Data fine")
+data_inizio_pm = st.date_input("Data inizio PM",key="data pm")
+data_fine_pm = st.date_input("Data fine PM", key="data pm")
 
 df_pm = db_pesoPersonale[
     (db_pesoPersonale["username"] == username)
