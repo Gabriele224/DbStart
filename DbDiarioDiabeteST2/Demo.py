5f26dfb35e0ec281412acb0fe4b3c17f0b388d6e
Gabriele224 <gabryricca2001@gmail.com>
2026-09-21 19:26:56 +0200

    Demo.py

--- DbDiarioDiabeteST2/Demo.py
+++ DbDiarioDiabeteST2/Demo.py
@@ -283,7 +283,7 @@ df_pasti = db_Pasto[
 
 df_alimento=db_alimento[
 
-    db_alimento["id_pasto"].isin(df_pasti["id_pasto"])
+    db_alimento["pastoId"].isin(df_pasti["id_pasto"])
 ]
 st.write("Tot Peso:", df_alimento["totPeso"].sum())
 st.write("Tot Cho:", df_alimento["totCho"].sum())
